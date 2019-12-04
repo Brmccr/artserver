f6ecb19a7c5d9a3484fad584f91d9fc0cd86f151
Jessica Cooke <jessicacooke@jessicas-mbp.attlocal.net>
2019-12-04 07:04:23 -0500

    commit

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 let express = require('express');
 let app = express();
+let user = require('./controllers/usercontroller')
 let art = require('./controllers/artcontroller')
+let comment = require('./controllers/commentcontroller')
 let db = require('./db');
 
 app.use(express.json());
@@ -11,7 +13,8 @@ db.sequelize.sync();
 app.use(require('./middleware/headers'));
 
 app.use('/art', art)
-
+app.use('/user', user)
+app.use('/comment', comment)
 app.use('/api/test', function(req, res) {
     res.send("This is data from the /api/test endpoint. It's from the server.");
 })
